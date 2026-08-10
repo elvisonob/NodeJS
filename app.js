f5aef0bd97a46395df7228bafbcd2825035bc823
elvisonob <elvisonob@yahoo.com>
2026-08-10 19:10:47 +0100

    splitting routes

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (res, req, next) => {
-  res.endo('One Love, two heart');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>The "Add Product" Page<h1>');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('This always runs');
+  next();
 });
 
 app.listen(3000);
