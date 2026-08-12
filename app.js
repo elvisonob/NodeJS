a80abe910edc1b109086fb5f4e1ec568bb13c133
elvisonob <elvisonob@yahoo.com>
2026-08-12 18:20:01 +0100

    backend HTML setup

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
-
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(201).send('Page not found');
+});
 
 app.listen(3000);
